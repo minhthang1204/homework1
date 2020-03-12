b7904e58e02660d0b9fe9d463c91124019933e79
minhthang1204 <61902004+minhthang1204@users.noreply.github.com>
2020-03-12 14:45:21 +0700

    Create homework2.java

---
+++ homework2.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Homework2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Ex1
+//        System.out.println("nhap vao a ");
+//
+//        System.out.println("nhap vao b ");
+        Scanner sc = new Scanner(System.in);
+        //        int a = sc.nextInt();
+        //        int b = sc.nextInt();
+        //
+        //        System.out.println("tong hai so la  " + (a + b));
+        // ex2
+        //        int n;
+        //        int sum = 0;
+        //        do {
+        //            System.out.print("nhap vao so n = ");
+        //            n = sc.nextInt();
+        //        } while (n <= 0);
+        //
+        //        for (int i = 1; i < n; i++) {
+        //            if (n % i == 0) {
+        //                sum += i;
+        //            }
+        //
+        //        }
+        //        if (n == sum) {
+        //            System.out.println("so do la so hoan hao ");
+        //        } else {
+        //            System.out.println("so do khong la so hoan hao");
+        //        }
+        //        sc.close();
+        //    }
+        //}
+        // EX3
+        //
+        //        int i, n;
+        //
+        //        long GT = 1;
+        //        do {
+        //            System.out.print("nhap vao n = ");
+        //            n = sc.nextInt();
+        //        } while (n < 0);
+        //
+        //        for (i = 1; i <= n; i++) {
+        //            GT = GT * i;
+        //        }
+        //        System.out.println(n + "! = " + GT);
+        //    }
+        //
+        //}
+        // EX4
+        //        int n, a, sum = 0;
+        //        System.out.println("nhap vao so do n=");
+        //        n = sc.nextInt();
+        //        while (n > 0) {
+        //            a = n % 10;
+        //            n = n / 10;
+        //            sum += a;
+        //        }
+        //
+        //        System.out.println("Tổng các chữ số = " + sum);
+        //    }
+        //}
+        // ex5
+//        System.out.print("nhap so a= ");
+//        int a = sc.nextInt();
+//        System.out.print("nhap so b= ");
+//        int b = sc.nextInt();
+//        System.out.println("uoc so chung lon nhat la" + USCLN(a, b));
+//  
+//
+//    public static int USCLN(int a, int b) {
+//        if (b == 0) {
+//            return a;
+//        }
+//        return USCLN(b, a % b);
+//    }
+//}
+//EX6
